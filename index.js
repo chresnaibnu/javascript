bceeec2659590284e6cfb0fe7aff9c7014ab9110
chresnaibnu <chresnaibnu@gmail.com>
2019-03-05 22:11:21 +0700

    Pengenalan var dalam Javascript

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 var message = "Hello Javascript";
+let msg1 = "Hello"
+const msg2 = "Coding"
+
+// message = "berubah message"
+
+if (true){
+  var message = 1;
+  let msg1 = 2;
+}
 
 console.log(message);
+console.log(msg1);
